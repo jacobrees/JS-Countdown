987a0e4ddee16e908c8451fe8e1d22da5280193f
jacobrees <jacob.rees97@yahoo.com>
2021-03-10 11:31:40 +0000

    Fix all css linter errors within project and tidy up all javascript code within project

--- app.js
+++ app.js
@@ -1,36 +1,36 @@
 const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
 ];
 const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
 ];
 
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let tempDate = new Date();
+const tempDate = new Date();
 
-let tempYear = tempDate.getFullYear();
-let tempMonth = tempDate.getMonth();
-let tempDay = tempDate.getDate();
+const tempYear = tempDate.getFullYear();
+const tempMonth = tempDate.getMonth();
+const tempDay = tempDate.getDate();
 
 const futureDate = new Date(tempYear, tempMonth, tempDay + 10, 11, 30, 0, 0);
 
@@ -41,45 +41,43 @@ const month = months[futureDate.getMonth()];
 const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 
-giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}am`
+giveaway.textContent = `giveaway ends on ${weekday} ${date} ${month} ${year} ${hours}:${minutes}am`;
 
 const futureTime = futureDate.getTime();
 
-function format(item){
-  if(item < 10){
+function format(item) {
+  if (item < 10) {
     return item = `0${item}`;
-  }else{
-    return item;
   }
+  return item;
 }
 
-function getRemainingTime(){
+function getRemainingTime() {
   const today = new Date().getTime();
   const t = futureTime - today;
 
-  const oneDay = 24*60*60*1000;
-  const oneHour = 60*60*1000;
-  const oneMinute = 60*1000;
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
 
-  const days = Math.floor(t/oneDay);
+  const days = Math.floor(t / oneDay);
   const hours = Math.floor((t % oneDay) / oneHour);
   const minutes = Math.floor((t % oneHour) / oneMinute);
-  const seconds = Math.floor((t % oneMinute) /1000);
+  const seconds = Math.floor((t % oneMinute) / 1000);
 
   const values = [days, hours, minutes, seconds];
 
-  
 
-  items.forEach(function (item, index) {
-    item.innerHTML= format(values[index])
-  })
+  items.forEach((item, index) => {
+    item.innerHTML = format(values[index]);
+  });
 
-  if(t<0){
-    clearInterval(countdown)
-    deadline.innerHTML = '<h4 class="expired">sorry this giveaway has expired</h4>'
+  if (t < 0) {
+    clearInterval(countdown);
+    deadline.innerHTML = '<h4 class="expired">sorry this giveaway has expired</h4>';
   }
 }
 
-let countdown = setInterval(getRemainingTime, 1000)
+let countdown = setInterval(getRemainingTime, 1000);
 
-getRemainingTime()
+getRemainingTime();
